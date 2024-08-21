7cd054e28d0e9c7994f009f89c14416137e62a3c
tavaresnicole <tavaresnicole05@gmail.com>
2024-08-20 23:02:30 -0300

    Adicionar arquivos de projeto.

---
+++ Modelos/Produto.cs
@@ -0,0 +1,17 @@
+﻿namespace Produtos.Modelos
+{
+    public class Produto
+    {
+        public Produto(int id, string nome, int quantidade) 
+        { 
+            Id = id;
+            Nome = nome;
+            Quantidade = quantidade;
+        }
+
+        public int Id;
+        public string Nome;
+        public int Quantidade;
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using Produtos.Modelos;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List <Produto> produtos = new List <Produto> ();
+
+        Console.WriteLine("Este é um sistema de inventário, digite:");
+        Console.WriteLine("1 - Adicionar produtos no inventário");
+        Console.WriteLine("2 - Ver relatório de produtos no inventário");
+        Console.WriteLine("3 - Remover produtos do inventário");
+        int opcao = int.Parse(Console.ReadLine()!);
+
+        while (opcao != 99)
+        {
+            if (opcao == 1)
+            {
+                Console.WriteLine("Digite o nome do produto:");
+                string nome = Console.ReadLine()!;
+                Console.WriteLine("Digite a quantidade do produto que você quer adicionar:");
+                string quantidade = Console.ReadLine()!;
+
+                Produto item = new Produto(produtos.Count() + 1, nome, int.Parse(quantidade));
+                produtos.Add(item);
+            }
+            else if (opcao == 2 && produtos.Count() >= 1) {
+                foreach (var item in produtos)
+                {
+                    Console.WriteLine($"Id: {item.Id}, Nome: {item.Nome} e Quantidade:{item.Quantidade}");
+                }
+            }
+            else if(opcao == 2 && produtos.Count() < 1)
+            {
+                Console.WriteLine("Ainda não há nada para ver no estoque.");
+            }
+            else if (opcao == 3 && produtos.Count() >= 1)
+            {
+                Console.WriteLine("Digite o id do produto que deseja remover do inventário:");
+                foreach (var item in produtos)
+                {
+                    Console.WriteLine($"Id: {item.Id}, Nome: {item.Nome} e Quantidade:{item.Quantidade}");
+                }
+                int id = int.Parse(Console.ReadLine()!);
+                Produto idProduto = produtos.Find(produto => id == produto.Id)!;
+                produtos.Remove(idProduto);
+            }
+            else if (opcao == 3 && produtos.Count() <= 0)
+            {
+                Console.WriteLine("Ainda não há nada para remover do estoque.");
+            }
+
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("1 - Adicionar produtos no inventário");
+            Console.WriteLine("2 - Ver relatório de produtos no inventário");
+            Console.WriteLine("3 - Remover produtos do inventário");
+            Console.WriteLine("---------------------------------------------");
+            opcao = int.Parse(Console.ReadLine()!);
+        }
+    }
+}
